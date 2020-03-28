852447c67a7de7f0ead94c4e8dc7b030c3b338f4
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-03-28 12:07:30 +0100

    Adjusted setup page;

--- src/components/pages/dotaOverlay/Configure.tsx
+++ src/components/pages/dotaOverlay/Configure.tsx
@@ -1,13 +1,44 @@
 import { ReactElement } from "react";
+import { OverlayMethods } from "./Configuration";
 
-export default function Configure(): ReactElement {
-    return <div className={'configOptions'}>
-        <div className={'col'}>
+function CheckBox({checked}: {checked: boolean}): ReactElement {
+    return <div className={'checkbox'}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <path fill={checked ? '#F15025' : '#CCC'} d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path>
+        </svg>
+
+        <style jsx>{`
+            .checkbox {
+                width: 40px;
+                margin-right: 10px;
+                flex-shrink: 0;
+                margin-top: -5px;
+            }    
+        `}</style>
+    </div>;
+}
+
+interface Props {
+    method: OverlayMethods;
+    setMethod: (method: OverlayMethods) => void;
+}
+
+export default function Configure({method, setMethod}: Props): ReactElement {
+    return <div className={'methodsContainer'}>
+        <div className={'info'}>
+            <h1>Dota Stats Overlay</h1>
+            <p>
+                Mit dem "Dota Stats Overlay" kannst du deinen aktuellen Stand deiner heutigen Dota Spiele anzeigen.
+            </p>
+        </div>
+
+        <div className={'method'} onClick={() => setMethod(OverlayMethods.dotaGsi)}>
+            <CheckBox checked={method === OverlayMethods.dotaGsi} />
             <div className={'option'}>
                 <h2>Dota GSI (Empfohlen)</h2>
                 <br />
-                <p className={'desc'}>Dota GSI (Dota Game State Integration) ist eine von Valve bereitgestellte Lösung um lokale Matchdetails mit einem Server zu synchronisieren. Die Kommunikation erfolgt ausschließlich vom Spiel zum Server und geht nicht umgekehrt. Außerdem empfangen wir nur die notwendigen Daten, heißt ob ein Spiel gewonnen oder verloren wurde.</p>
-                <p className={'speed'}>Das Update im Overlay erfolgt sobald das Spiel gewonnen bzw. verloren wurde.</p>
+                <p className={'desc'}>Dota GSI (Dota Game State Integration) ist eine von Valve bereitgestellte Lösung um lokale Matchdetails mit einem Server zu synchronisieren. Die Kommunikation erfolgt ausschließlich vom Spiel zum Server und geht nicht umgekehrt. Wir empfangen nur die notwendigen Daten, heißt ob ein Spiel gewonnen oder verloren wurde.</p>
+                <p className={'subDesc'}>Es gibt leider keine offizielle Dota 2 Dokumentation, weitere Details können aber in der CS:GO Dokumentation nachgelesen werden: <a href={'https://developer.valvesoftware.com/wiki/Counter-Strike:_Global_Offensive_Game_State_Integration'}>valvesoftware.com</a></p>
                 <br />
                 <div className={'list'}>Diese Methode bietet sich an, wenn:</div>
                 <ul>
@@ -17,13 +48,14 @@ export default function Configure(): ReactElement {
                 </ul>
             </div>
         </div>
-        <div className={'col'}>
+
+        <div className={'method'} onClick={() => setMethod(OverlayMethods.steam)}>
+            <CheckBox checked={method === OverlayMethods.steam} />
             <div className={'option'}>
                 <h2>Steam Accounts</h2>
                 <br />
                 <p className={'desc'}>Steam Accounts nutzt anhand der Steam id die öffentlichen API's von Stratz.com und/oder Dotabuff alle 2 Minuten befragt werden, ob ein Spiel beendet wurde oder nicht. Die Abfrage erfolgt in dem Falle nicht auf unserem Server sondern im Frame selber.</p>
                 <p className={'speed'}>Das Update im Overlay erfolgt alle 2 Minuten.</p>
-                <br />
                 <br />
                 <div className={'list'}>Diese Methode bietet sich an, wenn:</div>
                 <ul>
@@ -35,39 +67,32 @@ export default function Configure(): ReactElement {
         </div>
 
         <style jsx>{`
-            .configOptions {
+            .methodsContainer {
+                padding: 0 20px;
+            }
+
+            .method {
+                margin: 20px 0;
+                padding: 15px;
                 display: flex;
-                margin: -40px;
-            }
-
-            .col {
-                width: 50%;
-                padding: 40px;
-            }
-
-            .option {
+                border: 1px solid #CCC;
                 border-radius: 8px;
-                border: 1px solid #EEE;
-                padding: 12px 15px;
+                cursor: pointer;
+            }
+
+            .desc, .list, li {
+                font-size: 14px;
+            }
+            .subDesc {
+                font-size: 13px;
+            }
+
+            .list {
+                margin-bottom: 5px;
             }
 
             ul {
-                padding-left: 25px;
-                margin-top: 10px;
-            }
-
-            li, .list {
-                padding: 5px 0;
-                color: #555;
-            }
-
-            .desc {
-                font-style: italic;
-                color: #777;
-            }
-
-            .speed {
-                color: #999;
+                padding-left: 20px;
             }
         `}</style>
     </div>;
