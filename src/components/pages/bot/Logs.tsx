485bbc96f085ad1645362b85c7d6d8fe218466c3
Michael Ketzer <micha@Michaels-MacBook-Pro.local>
2020-04-09 17:10:04 +0200

    Added live log;

---
+++ src/components/pages/bot/Logs.tsx
@@ -0,0 +1,49 @@
+import { ReactElement, useMemo, useRef, useEffect } from "react";
+import { useStateValue } from "../../context/websocket/context";
+import { ChatMessage, Message } from "../../context/websocket/state";
+
+function isChatMessage(message: Message): message is ChatMessage {
+    return message.type === 'chat';
+}
+export default function Logs(): ReactElement {
+    const [{messages}] = useStateValue();
+    const ref = useRef<HTMLDivElement>();
+
+    const messagesByType = useMemo(() => messages.filter(isChatMessage), [messages]);
+
+    useEffect(() => {
+        if(ref && ref.current) {
+            ref.current.scrollTop = 1000000;
+        }
+
+    }, [ref, messagesByType])
+
+    return <div className={'container'}>
+        <h2>Live Log Einträge</h2>
+        <div className={'logsContainer'} ref={ref}>
+            {messagesByType.map(({value: {message, user}}, index) => <div key={index} className={'msg'}>
+                <span>{user}</span>: <span>{message}</span>
+            </div>)}
+        </div>
+
+        <style jsx>{`
+            .container {
+                margin: 20px 0;
+            }
+            
+            .logsContainer {
+                background: #000;
+                color: #FFF;
+                padding: 20px;
+                min-height: 200px;
+                border-radius: 8px;
+                overflow-y: scroll;
+                max-height: 200px;
+            }
+
+            .msg {
+                font-family: monospace;
+            } 
+        `}</style>
+    </div>;
+}
