28e0aefc5737490fb1116be52e7ea6de0c9f2e8d
Michael Ketzer <micha@Michaels-MacBook-Pro.local>
2020-04-12 09:02:53 +0200

    Adjusted design and rebranded channel bot name; Removed unnecessary stuff;

--- src/components/pages/bot/basic.tsx
+++ src/components/pages/bot/basic.tsx
@@ -3,7 +3,7 @@ import { useAbortFetch } from "../../../hooks/abortFetch";
 import { fetchBotConfig, patchBotConfig, fetchCurrentUser } from "../../../api/user";
 import { BotData } from "../../../api/@types/User";
 import Loader from "../../Loader";
-import { Checkbox, Input } from "antd";
+import { Checkbox, Input, Alert } from "antd";
 import { UserOutlined } from '@ant-design/icons';
 import ContextProvider from "../../context/websocket/context";
 import { initialState, reducer } from "../../context/websocket/state";
@@ -16,20 +16,19 @@ export default function Basic(): ReactElement {
 
     if(config) {
         return <div className={'wrapper'}>
-            <Checkbox defaultChecked={config.useBot} onChange={(e) => patch({useBot: e.target.checked})}>Channel Bot aktivieren</Checkbox>
+            <div>StreamDota Bot ist ein Chatbot welcher dir Unterhaltung, Informationen zum aktuellen Dota2 Spiel und Interaktionen mit dem Chat bietet.</div>
+
+            <br />
+            <Checkbox defaultChecked={config.useBot} onChange={(e) => patch({useBot: e.target.checked})}>StreamDota Bot aktivieren</Checkbox>
             <br />
             <br />
-            <div><b>Custom Bot Config</b></div>
-            <div className={'inputGrid'}>
-                <Input placeholder={'Bot Name'} 
-                       prefix={<UserOutlined />} 
-                       defaultValue={config.customBotName} 
-                       onBlur={(e) => patch({customBotName: e.target.value})} />
-                <Input placeholder={'Bot OAuth Token'} 
-                       prefix={<UserOutlined />} 
-                       defaultValue={config.customBotToken} 
-                       onBlur={(e) => patch({customBotToken: e.target.value})} />
-            </div>
+            <Alert message={<>
+                Vergess nicht den Bot zum Mod zu machen, das verhindert Probleme mit dem Chat. Einfach "<span style={{fontFamily: 'monospace'}}>/mod streamdotade</span>" im Chat schreiben!
+            </>} type={'warning'} />
+            <br />
+            <Alert message={<>
+                Du möchtest deinen eigenen Namen für den Bot haben? Dann schreib mich einfach bei Discord an! (<a href={'https://discordapp.com/channels/@me/148698273899610112/'}>GriefCode#1337</a>)
+            </>} type={'info'} />
 
             {user && <ContextProvider initialState={initialState} reducer={reducer} url={'wss://api.streamdota.de/user/bot/live/' + user.frameApiKey}>
                 <Logs />
---
+++ src/pages/bets.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from "react";
+import PageFrame from "../components/PageFrame";
+
+export default function Bets(): ReactElement {
+    return <PageFrame title={'Wetten'}>
+        Wetten
+    </PageFrame>
+}
