4a8a1e6d218558dbdc97d764300544a4efef2186
Michael Ketzer <micha@Michaels-MacBook-Pro.local>
2020-04-18 20:37:25 +0200

    Updated commands

--- src/components/pages/bot/CommandList.tsx
+++ src/components/pages/bot/CommandList.tsx
@@ -3,14 +3,16 @@ import { useAbortFetch } from "../../../hooks/abortFetch";
 import { getCommands, updateCommand, createCommand, deleteCommand } from "../../../api/command";
 import { Command } from "../../../api/@types/Command";
 import React from "react";
-import { Input, Button, Popconfirm } from "antd";
+import { Input, Button, Popconfirm, Checkbox } from "antd";
 import {DeleteOutlined} from '@ant-design/icons';
 import Loader from "../../Loader";
+import TextArea from "antd/lib/input/TextArea";
 
 export default function CommandList(): ReactElement {
     const [commands, load] = useAbortFetch<Command[]>(getCommands);
     const [cmd, setCmd] = useState('');
     const [msg, setMsg] = useState('');
+    const [act, setAct] = useState(false);
 
     const create = useCallback(async () => {
         if(msg.length > 0 && cmd.length > 0) {
@@ -23,15 +25,26 @@ export default function CommandList(): ReactElement {
 
     if(commands) {
         return <div className={'commandsGrid'}>
-            {commands.map(({id, command, message}) => <React.Fragment key={id}>
+            <div className={'label'}>Aktiv</div>
+            <div className={'label'}>Command</div>
+            <div className={'label'}>Antwort</div>
+            <div className={'label'}>Vorschau</div>
+
+            {commands.map(({active, id, command, message}) => <React.Fragment key={id}>
+                <div className={'activeBox'}>
+                    <Checkbox defaultChecked={active} onChange={async (e) => {
+                        await updateCommand(id, e.target.checked, command, message);
+                        await load();
+                    }}/>
+                </div>
                 <Input defaultValue={command} onBlur={async (e) => {
-                    await updateCommand(id, e.target.value, message);
+                    await updateCommand(id, active, e.target.value, message);
                     await load();
-                }} placeholder={'Command'} />
+                }} />
                 <Input defaultValue={message} onBlur={async (e) => {
-                    await updateCommand(id, command, e.target.value);
+                    await updateCommand(id, active, command, e.target.value);
                     await load();
-                }} placeholder={'Antwort'} />
+                }} />
                 <div>
     
                     <Popconfirm title="Soll dieser Command wirklich gelöscht werden?" onConfirm={async () => {
@@ -43,16 +56,42 @@ export default function CommandList(): ReactElement {
                 </div>
             </React.Fragment>)}
     
-            <Input value={cmd} onChange={(e) => setCmd(e.target.value)} placeholder={'Command'} />
-            <Input value={msg} onChange={(e) => setMsg(e.target.value)} placeholder={'Antwort'} />
-            <div><Button type={'primary'} onClick={create}>Erstellen</Button></div>
+            <div className={'activeBox'}>
+                <Checkbox defaultChecked={act} onChange={(e) => setAct(e.target.checked)}/>
+            </div>
+            <div>
+                <Input value={cmd} onChange={(e) => setCmd(e.target.value)} placeholder={'Command'} />
+            </div>
+            <TextArea value={msg} onChange={(e) => setMsg(e.target.value)} placeholder={'Antwort'} />
+            <div></div>
+            <div />
+            <div />
+            <div className={'createButton'}>
+                <Button type={'primary'} onClick={create}>Erstellen</Button>
+            </div>
     
             <style jsx>{`
+                .activeBox {
+                    height: 32px;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                }
+
+                .label {
+                    margin-bottom: -15px;
+                    margin-top: 10px;
+                }
+
                 .commandsGrid {
                     display: grid;
-                    grid-template-columns: 200px 400px 100px;
+                    grid-template-columns: max-content 170px 330px 1fr;
                     grid-column-gap: 20px;
                     grid-row-gap: 15px;
+                }
+
+                .createButton {
+                    justify-self: flex-end;
                 }
             `}</style>
         </div>;
