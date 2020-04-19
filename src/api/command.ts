1dcfb744640f464587d7063b43cb2f842ee6c384
Michael Ketzer <micha@Michaels-MacBook-Pro.local>
2020-04-19 13:51:34 +0200

    Updated commands for bots;

--- src/api/command.ts
+++ src/api/command.ts
@@ -2,17 +2,17 @@ import { Command } from "./@types/Command";
 import { get, patch, create, del } from "./request";
 
 export async function getCommands(abortController: AbortController): Promise<Command[]> {
-    return await get<Command[]>('/user/commands', 'json', {signal: abortController.signal});
+    return await get<Command[]>('/command/list', 'json', {signal: abortController.signal});
 }
 
-export async function createCommand(command: string, message: string): Promise<Command[]> {
-    return await create('/user/command', {command, message});
+export async function createCommand(active: boolean, command: string, message: string): Promise<Command[]> {
+    return await create('/command/create', {active, command, message});
 }
 
 export async function updateCommand(id: number, active: boolean, command: string, message: string): Promise<Command[]> {
-    return await patch('/user/command/' + id, {active, command, message});
+    return await patch('/command/' + id, {active, command, message});
 }
 
 export async function deleteCommand(id: number): Promise<Command[]> {
-    return await del('/user/command/' + id);
+    return await del('/command/' + id);
 }
--- src/components/pages/bot/basic.tsx
+++ src/components/pages/bot/basic.tsx
@@ -1,36 +1,27 @@
 import { ReactElement } from "react";
-import { useAbortFetch } from "../../../hooks/abortFetch";
-import { fetchBotConfig } from "../../../api/user";
-import Loader from "../../Loader";
 import { Alert, Typography } from "antd";
 
 export default function Basic(): ReactElement {
-    const [config] = useAbortFetch(fetchBotConfig);
+    return <div className={'wrapper'}>
+        <Alert message={<>
+            Vergess nicht den Bot zum Mod zu machen, das verhindert Probleme mit dem Chat. Einfach <span style={{fontFamily: 'monospace'}}><Typography.Text code>/mod streamdotade</Typography.Text></span> im Chat schreiben!
+        </>} type={'warning'} />
+        <br />
+        <Alert message={<>
+            Du möchtest deinen eigenen Namen für den Bot haben? Dann schreib mich einfach bei Discord an! (<a href={'https://discordapp.com/channels/@me/148698273899610112/'}>GriefCode#1337</a>)
+        </>} type={'info'} />
 
-    if(config) {
-        return <div className={'wrapper'}>
-            <Alert message={<>
-                Vergess nicht den Bot zum Mod zu machen, das verhindert Probleme mit dem Chat. Einfach <span style={{fontFamily: 'monospace'}}><Typography.Text code>/mod streamdotade</Typography.Text></span> im Chat schreiben!
-            </>} type={'warning'} />
-            <br />
-            <Alert message={<>
-                Du möchtest deinen eigenen Namen für den Bot haben? Dann schreib mich einfach bei Discord an! (<a href={'https://discordapp.com/channels/@me/148698273899610112/'}>GriefCode#1337</a>)
-            </>} type={'info'} />
+        <style jsx>{`
+            .wrapper {
+                max-width: 800px;
+            }
 
-            <style jsx>{`
-                .wrapper {
-                    max-width: 800px;
-                }
-
-                .inputGrid {
-                    display: grid;
-                    align-items: center;
-                    grid-template-columns: 1fr 1fr;
-                    grid-column-gap: 20px;
-                }    
-            `}</style>
-        </div>;
-    }
-
-    return <Loader />;
+            .inputGrid {
+                display: grid;
+                align-items: center;
+                grid-template-columns: 1fr 1fr;
+                grid-column-gap: 20px;
+            }    
+        `}</style>
+    </div>;
 }
