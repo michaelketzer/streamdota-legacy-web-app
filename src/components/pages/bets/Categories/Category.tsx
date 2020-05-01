2f50ffa4121a9c262613426b6a85c160a5adda8c
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-05-01 15:16:03 +0200

    Adjusted structure; Added invite list for bet seasons;

--- src/components/pages/bets/Categories/Category.tsx
+++ src/components/pages/bets/Categories/Category.tsx
@@ -1,25 +1,38 @@
-import { ReactElement } from "react";
+import { ReactElement, useMemo } from "react";
 import { useAbortFetch } from "../../../../hooks/abortFetch";
 import { BetSeason } from "../../../../api/@types/BetSeason";
 import { fetchUserBetSeasons } from "../../../../api/betSeason";
-import { Typography, Row, Col } from "antd";
+import { Typography, Row, Col, Space } from "antd";
 import CurrentCategory from "./CurrentCategory";
 import CategoryList from "./CategoryList";
+import { User } from "../../../../api/@types/User";
+import { fetchCurrentUser } from "../../../../api/user";
+import SeasonInvites from "./SeasonInvites";
 
 export interface CategoryProps {
     seasons: BetSeason[];
     reload?: () => void;
+    currentBetSeason?: number;
 }
 
 export default function Category(): ReactElement {
     const [seasons, load] = useAbortFetch<BetSeason[]>(fetchUserBetSeasons);
+    const [user, reloadUser] = useAbortFetch<User>(fetchCurrentUser);
+    const currentSeasonName = useMemo(() => (user && seasons && seasons.find(({id}) => id === user.betSeasonId).name) || '', [user, seasons]);
 
     return <>
         <Typography.Paragraph>Kategorien ist gleichzusetzen mit einer “Season”. Wettrunden werden immer für eine aktuell ausgewählte Kategorie  gestartet. Du kannst hier alle Kategorien verwalten.</Typography.Paragraph>
+
+        <div style={{margin: '30px 0'}} />
+
         <Row gutter={[50, 50]}>
             <Col span={12}>
-                <CurrentCategory seasons={seasons} reload={load} />
-                <CategoryList seasons={seasons} reload={load} />
+                <CurrentCategory seasons={seasons} reload={load} currentBetSeason={user && user.betSeasonId} onChange={reloadUser} />
+                <CategoryList seasons={seasons} reload={load} currentBetSeason={user && user.betSeasonId} />
+            </Col>
+
+            <Col span={12}>
+                {user && <SeasonInvites name={currentSeasonName} seasonId={user.betSeasonId} />}
             </Col>
         </Row>
     </>;
--- src/components/pages/bets/Categories/CategoryList.tsx
+++ src/components/pages/bets/Categories/CategoryList.tsx
@@ -11,19 +11,30 @@ import ListItem from "./ListItem";
 
 export const emptySeason: () => Partial<BetSeason> = () => ({id: null, name: '', description: '', type: 'tournament'});
 
-export default function CategoryList({seasons, reload}: CategoryProps): ReactElement {
+export default function CategoryList({seasons, reload, currentBetSeason}: CategoryProps): ReactElement {
     const [category, setCategory] = useState<Partial<BetSeason> | null>(null);
 
     if(seasons) {
         return <>
             <List style={{marginTop: '20px', minWidth: '400px'}} pagination={{defaultCurrent: 1, position: 'bottom', pageSize: 3}} dataSource={seasons}
-            renderItem={(season) => <ListItem season={season} onEdit={() => setCategory(season)} onDelete={async () => {
+            renderItem={(season) => <ListItem key={season.id} activeSeason={currentBetSeason} season={season} onEdit={() => setCategory(season)} onDelete={async () => {
                 await deleteSeason(season.id);
                 await reload();
             }} />} />
 
-            <Button type={'primary'} size={'middle'} icon={<PlusOutlined />} onClick={() => setCategory(emptySeason())}>Neue Kategorie</Button>
+            <div className={'buttons'}>
+                <Button type={'primary'} size={'middle'} icon={<PlusOutlined />} onClick={() => setCategory(emptySeason())}>Neue Kategorie</Button>
+                <Button type={'dashed'} size={'middle'} icon={<PlusOutlined />} disabled>Einladung annehmen</Button>
+            </div>
             <CategoryPopup season={category} onFinish={() => reload()}/>
+
+            <style jsx>{`
+                .buttons {
+                    display: grid;
+                    grid-template-columns: max-content max-content 1fr;
+                    grid-column-gap: 25px;
+                }    
+            `}</style>
         </>;
     }
 
