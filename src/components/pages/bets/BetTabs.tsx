1304a6e6be89facb34a175be24267426dfa78907
Michael Ketzer <micha@Michaels-MacBook-Pro.local>
2020-05-03 14:06:48 +0200

    Added bet rounds;

--- src/components/pages/bets/BetTabs.tsx
+++ src/components/pages/bets/BetTabs.tsx
@@ -3,9 +3,10 @@ import { Tabs } from "antd";
 import Dashboard from "./Dashboard";
 import BotCommands from "./BotCommands";
 import CategoryTab from "./Categories/Tab";
+import BetRoundsTab from "./BetRounds/Tab";
 
 export default function BetTabs(): ReactElement {
-    return <Tabs defaultActiveKey="dashboard" animated={false}>
+    return <Tabs defaultActiveKey="dashboard" animated={false} destroyInactiveTabPane>
         <Tabs.TabPane tab="Dashboard" key="dashboard">
             <Dashboard />
         </Tabs.TabPane>
@@ -13,7 +14,7 @@ export default function BetTabs(): ReactElement {
             <CategoryTab />
         </Tabs.TabPane>
         <Tabs.TabPane tab="Runden" key="rounds">
-            Runden
+            <BetRoundsTab />
         </Tabs.TabPane>
         <Tabs.TabPane tab="Streamdota Bot Commands" key="commands">
             <BotCommands />
