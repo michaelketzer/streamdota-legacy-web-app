1974ebdc869fbb9f94939dd323f7da18aa5fb5e4
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-05-10 10:12:48 +0200

    Fixed namin;

--- src/components/pages/dotaOverlay/Overlay/FrameLink.tsx
+++ src/components/pages/dotaOverlay/Overlay/FrameLink.tsx
@@ -3,7 +3,7 @@ import Paragraph from "antd/lib/typography/Paragraph";
 import { User } from "../../../../api/@types/User";
 
 export default function FrameLink({userData}: {userData: User | null}): ReactElement {
-    const text = 'https://streamdota.de/frames/dotaStats?auth=' + (userData && userData.frameApiKey);
+    const text = 'https://streamdota.com/frames/dotaStats?auth=' + (userData && userData.frameApiKey);
 
     return <>
         <div><b>Overlay Frame Source</b></div>
