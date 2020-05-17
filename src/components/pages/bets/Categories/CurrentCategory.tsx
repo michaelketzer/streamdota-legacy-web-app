cf3e92e4e62c96ebd86413f298feec762e34c013
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-05-17 11:00:47 +0200

    Fixed new account case for bettings;

--- src/components/pages/bets/Categories/CurrentCategory.tsx
+++ src/components/pages/bets/Categories/CurrentCategory.tsx
@@ -5,7 +5,7 @@ import Loader from "../../../Loader";
 import { CategoryProps } from "./Category";
 
 export default function CurrentCategory({seasons, currentBetSeason, onChange}: CategoryProps & {onChange: () => void}): ReactElement {
-    if(currentBetSeason && seasons) {
+    if(seasons) {
         return <>
             <Typography.Text strong>Aktuelle Kategorie</Typography.Text><br />
             <Select defaultValue={currentBetSeason} style={{width: '200px'}} onChange={async (betSeasonId) => {
