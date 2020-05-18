fd8de39e5f01220f4258ec4cebc4908ac7f252ab
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-05-18 11:55:43 +0200

    Added sentry reporting;

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,9 @@
+import * as Sentry from '@sentry/browser';
 import 'antd/dist/antd.min.css';
+
+if(process.env.SENTRY_DSN.length > 0 ) {
+    Sentry.init({dsn: process.env.SENTRY_DSN});
+}
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
