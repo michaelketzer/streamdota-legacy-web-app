bf7fc5e31142d61adb512ca76ff0d0c76789fff8
Michael Ketzer <mk@Michaels-MacBook-Pro.local>
2020-06-28 16:26:19 +0200

    Added timer preview;

--- src/components/pages/bets/Overlay/Timer.tsx
+++ src/components/pages/bets/Overlay/Timer.tsx
@@ -6,6 +6,7 @@ import { patchBetOverlay } from "../../../../modules/reducer/BetOverlay";
 import { Typography } from "antd";
 import Color from "../../dotaOverlay/Overlay/Color";
 import FontSize from "../../dotaOverlay/Overlay/FontSize";
+import TimerCounter from "./TimerCounter";
 
 export default function Timer(): ReactElement {
     const overlay = useBetOverlay();
@@ -15,18 +16,25 @@ export default function Timer(): ReactElement {
         dispatch(patchBetOverlay(data));
     }, [dispatch]);
     
-    return <div className={'topGrid'}>
-        <div>
-            <Typography.Title level={3}>Farbe</Typography.Title>
-            <Color label={'Hintergrund'} disableAlpha={false} value={overlay.timerBackground} setValue={(timerBackground) => patch({timerBackground})}/>
-            <Color label={'Schrift'} value={overlay.timerFont} setValue={(timerFont) => patch({timerFont})}/>
+    return <div>
+        <div className={'topGrid'}>
+            <div>
+                <Typography.Title level={3}>Farbe</Typography.Title>
+                <Color label={'Hintergrund'} disableAlpha={false} value={overlay.timerBackground} setValue={(timerBackground) => patch({timerBackground})}/>
+                <Color label={'Schrift'} value={overlay.timerFont} setValue={(timerFont) => patch({timerFont})}/>
+            </div>
+
+            <div>
+                <Typography.Title level={3}>Schrift</Typography.Title>
+
+                <FontSize fontSize={overlay.timerFontSize} setFontSize={(timerFontSize) => patch({timerFontSize})} />
+
+            </div>
         </div>
 
-        <div>
-            <Typography.Title level={3}>Schrift</Typography.Title>
-
-            <FontSize fontSize={overlay.timerFontSize} setFontSize={(timerFontSize) => patch({timerFontSize})} />
-
+        <div className={'preview'}>
+            <img className={'exampleBackground'} src={'/images/example_background.png'} />
+            <div className={'timer'}><TimerCounter overlay={overlay} /></div>
         </div>
 
         <style jsx>{`
@@ -34,6 +42,29 @@ export default function Timer(): ReactElement {
                 display: grid;
                 grid-template-columns: max-content max-content;
                 grid-column-gap: 100px;
+            }
+
+            .preview {
+                position: relative;
+                min-width: 400px;
+                padding: 20px;
+                padding-top: 7rem;
+                margin-top: 80px;
+            }
+
+            .exampleBackground {
+                position: absolute;
+                top: 0;
+                left: 0;
+                right: 0;
+                object-fit: cover;
+                object-position: 100%% 100%;
+                width: 100%;
+            }
+
+            .timer {
+                position: relative;
+                display: flex;
             }
         `}</style>
     </div>
---
+++ src/components/pages/bets/Overlay/TimerCounter.tsx
@@ -0,0 +1,29 @@
+import { ReactElement } from "react";
+import { BetOverlay } from "@streamdota/shared-types";
+import GoogleFontLoader from "react-google-font-loader";
+
+interface Props {
+    overlay: BetOverlay;
+}
+
+export default function TimerCounter({overlay}: Props): ReactElement {
+    return <div className={'wrapper'}>
+        {overlay.fontFamily && <GoogleFontLoader fonts={[{font: overlay.fontFamily, weights: [overlay.fontVariant]}]} />}
+
+        <div className={'counter'}>1:20</div>
+
+        <style jsx>{`
+            .wrapper {
+                background-color: ${overlay.timerBackground};
+                font-size: ${overlay.timerFontSize}px;
+                padding: .5em .75em;
+                line-height: 1em;
+            }
+
+            .counter {
+                font-family: ${overlay.fontFamily};
+                color: ${overlay.timerFont};
+            }
+        `}</style>
+    </div>;
+}
