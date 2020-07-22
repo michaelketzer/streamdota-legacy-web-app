a16cb9b0ae022bcda0d3cab5aeb99b5455f1e884
Michael Ketzer <mk@Michaels-MacBook-Pro.local>
2020-07-22 20:36:37 +0200

    Added slide bubble;

--- src/components/pages/index/Slides/BettingSlider.tsx
+++ src/components/pages/index/Slides/BettingSlider.tsx
@@ -6,9 +6,22 @@ const variants = {
     show: {scale: 1, transition: {delay: 3.5}},
 }
 
+const bubble = {
+    hidden: {scale: 0},
+    show: {
+        scale: 1, 
+        transition: {
+            delay: 3.8
+        }
+    },
+}
+
 export default function BettingSlider(): ReactElement {
     return <div className={'sliderContainer'}>
-        <motion.div className={'slider'} initial={'hidden'} animate={'show'} variants={variants} />
+        <motion.div className={'slider'} initial={'hidden'} animate={'show'} variants={variants}>
+            <motion.div className={'sliderbubble'} variants={bubble} initial={'hidden'} animate={'show'} />
+        </motion.div>
+
 
         <style jsx global>{`
             .sliderContainer {
@@ -25,6 +38,19 @@ export default function BettingSlider(): ReactElement {
                 width: 30rem;
                 border-radius: 1.5rem;
                 background-color: #FFF;
+                position: relative;
+            }
+
+            .sliderbubble {
+                background: linear-gradient(to bottom, rgba(100,100,100,1) 0%,rgba(14,14,14,1) 100%);
+                height: 3.4rem;
+                width: 3.4rem;
+                border-radius: 1.7rem;
+                border: 2px solid #CCC;
+                margin-top: -0.2rem;
+                box-shadow: 1px 1px 20px 0 rgba(0,0,0,0.4);
+                position: absolute;
+                left: 13.3rem;
             }
         `}</style>
     </div>;
