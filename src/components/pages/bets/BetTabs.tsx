c881434c033d39545a3750a85f305c8a32dc2d88
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-08-10 12:02:55 +0000

    Update BetTabs.tsx

--- src/components/pages/bets/BetTabs.tsx
+++ src/components/pages/bets/BetTabs.tsx
@@ -21,7 +21,7 @@ export default function BetTabs(): ReactElement {
         <Tabs.TabPane tab="Toplist" key="toplist">
             <ToplistTab />
         </Tabs.TabPane>
-        <Tabs.TabPane tab="Streamdota Bot Commands" key="commands">
+        <Tabs.TabPane tab="TinkBot Command" key="commands">
             <BotCommands />
         </Tabs.TabPane>
         <Tabs.TabPane tab="Overlay Einstellungen" key="overlay">
