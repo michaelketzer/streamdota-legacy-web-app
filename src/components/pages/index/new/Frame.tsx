f0818f6ba8f2d1ea9653f4cfc9988c4e37f12734
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-08-18 16:21:32 +0200

    Adjusted landing page and colorings;

--- src/components/pages/index/new/Frame.tsx
+++ src/components/pages/index/new/Frame.tsx
@@ -1,9 +1,32 @@
 import { ReactElement, ReactNode } from "react";
+import classNames from "classnames";
 
-export default function Frame({children, id}: {children: ReactNode; id?: string;}): ReactElement {
-    return <div className={'frame'} id={id}>
-        {children}
+interface Props {
+    blue?: boolean;
+    children: ReactNode;
+    grey?: boolean;
+    orange?: boolean;
+    id?: string;
+}
+
+export default function Frame({blue, children, grey, id, orange}: Props): ReactElement {
+    return <div className={classNames('wrapper', {blue, grey, orange})}>
+        <div className={'frame'} id={id}>
+            {children}
+        </div>
         <style jsx>{`
+            .grey {
+                background-color: #eee;
+            }
+
+            .blue {
+                background-color: #f2fcff;
+            }
+
+            .orange {
+                background-color: #fff8f2;
+            }
+
             .frame {
                 max-width: 1200px;
                 width: 100%;
