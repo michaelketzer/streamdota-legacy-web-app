abffa7cd07a3cb70498e8a2c72d51bd164d73676
Michael Ketzer <mk@Michaels-MacBook-Pro.local>
2020-08-19 13:54:30 +0200

    Translated panel to german;

--- src/pages/live.tsx
+++ src/pages/live.tsx
@@ -11,16 +11,16 @@ import LiveFeed from "../components/pages/live/LiveFeed";
 
 export default function Live(): ReactElement {
     const user = useCurrentUser();
-    const [leagueId, setLeagueId] = useState(12229);
+    const [leagueId, setLeagueId] = useState(0);
     
     return <PageFrame title={'Live Feed'}>
         <PageHeader 
             title={'Live Feed'} 
-            description={'Gathers data about current ingame actions, such as picks, bans & items and allows casters to display detailed stats on these actions.'} />
+            description={'Zeigt Daten zu aktuellen in-game Ereignissen an, wie Picks und Bans, mit weitere Daten der aktuellen Liga'} />
 
         <Alert message={
             <>
-                Source league id should be the id of the league where the data is gathered from. The stratz API is used to gain these details. You can search for the revelant league here: <span className={'highlight'}>https://stratz.com/leagues</span>. By clicking on the league you will find the id in the url, like here: https://stratz.com/leagues/<span className={'highlight'}>11629</span>
+                Die Liga ID ist die Liga von der die Daten zu Picks & Bans angezeigt wird. Dazu benutzen wir die stratz API. Die ID musst du leider selbst ermitteln unter folgendem Link: <span className={'highlight'}>https://stratz.com/leagues</span>. Einfach auf die entsprechende Liga klicken und aus der Browser URL die letzte Zahl kopieren, wie zB hier: https://stratz.com/leagues/<span className={'highlight'}>11629</span>
             </>
         } type="info" />
 
