cc2b472ffcb9788a03ac067301545f4afd41d652
Michael Ketzer <mk@Michaels-MacBook-Pro.local>
2020-08-23 18:34:13 +0200

    Recheck..

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import * as Sentry from '@sentry/browser';
-import { Store } from 'redux';
-import App, { AppInitialProps } from 'next/app';
+import { AppProps } from 'next/app';
 import 'antd/dist/antd.min.css';
 import 'react-typist/dist/Typist.css';
-import { State, wrapper } from '../modules/Store';
+import { wrapper } from '../modules/Store';
 import i18n from '../i18n'
 import { Integrations } from '@sentry/apm';
+import { FC } from 'react';
 
 if (process.env.SENTRY_DSN && process.env.SENTRY_DSN.length > 0) {
     Sentry.init({
@@ -17,11 +17,8 @@ if (process.env.SENTRY_DSN && process.env.SENTRY_DSN.length > 0) {
     });
 }
 
-class MyApp extends App<AppInitialProps & { store: Store<State> }> {
-	render() {
-		const { Component, pageProps } = this.props;
-		return <Component {...pageProps} />;
-	}
-}
+const WrappedApp: FC<AppProps> = ({Component, pageProps}) => (
+    <Component {...pageProps} />
+);
 
-export default wrapper.withRedux(i18n.appWithTranslation(MyApp));
+export default wrapper.withRedux(i18n.appWithTranslation(WrappedApp));
