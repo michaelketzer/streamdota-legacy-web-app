c07cbdb1fa250f4611adb0de04a89e435bcd751f
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-08-29 11:58:36 +0000

    Further testing

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,26 +1,27 @@
 import Router from 'next/router';
 import Loader from '../components/Loader';
 import { useDispatch } from 'react-redux';
-import { Dispatch } from 'react';
+import { Dispatch, useEffect } from 'react';
 import { authUser } from '../modules/reducer/User';
 import { wrapper } from '../modules/Store';
+import { useRouter } from "next/router";
 
 async function handleAuthRoutine(dispatch: Dispatch<any>, code: string): Promise<void> {
     const success = (await dispatch(authUser(code))) as unknown as boolean;
     Router.push(success ? '/dashboard' : '/');
 }
 
-const Auth = ({query}: {query: any}) => {
+const Auth = () => {
+    const router = useRouter();
     const dispatch = useDispatch();
-    console.log(process.browser, query);
-    if(process.browser && query.code) {
-        handleAuthRoutine(dispatch, query.code);
-    }
+    useEffect(() => {
+        console.log(process.browser, router.query);
+        if(process.browser && router.query.code) {
+            handleAuthRoutine(dispatch, router.query.code);
+        }
+    }, [router.query]);
+    
     return <Loader />;
-}
-
-Auth.getInitialProps = ({query}) => {
-    return {query}
 }
 
 export default wrapper.withRedux(Auth);
