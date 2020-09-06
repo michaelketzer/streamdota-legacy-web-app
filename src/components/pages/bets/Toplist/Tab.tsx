75e80fb199ab7ca4929d0bacc803427a391e78ab
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-09-06 12:18:50 +0200

    Added option to draw a random winner;

--- src/components/pages/bets/Toplist/Tab.tsx
+++ src/components/pages/bets/Toplist/Tab.tsx
@@ -2,31 +2,43 @@ import { ReactElement, useState } from "react";
 import CategorySelect, { useSelectedCategory } from "../CategorySelect";
 import Loader from "../../../Loader";
 import ToplistTable from "./ ToplistTable";
-import { Input } from "antd";
+import { Input, Popover, Button } from "antd";
 import { WithTranslation } from "next-i18next";
 import i18nInstance from "../../../../i18n";
+import DrawWinner from "./DrawWinner";
 
 const Tab = ({t}: WithTranslation): ReactElement => {
     const {category, setCategory} = useSelectedCategory();
     const [search, setSearch] = useState('');
+    const [drawWinner, setDrawWinner] = useState(false);
 
     if(category) {
         return <>
             <div className={'filter'}>
-                <div className={'search'}>
-                    <CategorySelect season={category} setSeason={setCategory} />
+                <div className={'row'}>
+                    <div className={'search'}>
+                        <CategorySelect season={category} setSeason={setCategory} />
+                    </div>
+                    <Input.Search onChange={(e) => setSearch(e.target.value)}  placeholder={t('bet-season-toplist-search')} style={{width: '200px'}} />
                 </div>
-                <Input.Search onChange={(e) => setSearch(e.target.value)}  placeholder={t('bet-season-toplist-search')} style={{width: '200px'}} />
+                <div className={'randomWinner'}>
+                    <Button onClick={() => setDrawWinner(true)}>{t('bet-toplist-random-winner')}</Button>
+                    <DrawWinner show={drawWinner} setShow={setDrawWinner} season={category} />
+                </div>
             </div>
             <br />
             <br />
             <ToplistTable season={category} search={search} />
 
             <style jsx>{`
-                .filter {
+                .filter, .row {
                     display: flex;
                     align-items: flex-end;
                 }  
+
+                .filter {
+                    justify-content: space-between;
+                }
 
                 .search {
                     margin-right: 20px;
