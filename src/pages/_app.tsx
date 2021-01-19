0d950284f0f94e5c5b91a600e107a6f623892add
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-01-19 21:57:34 +0100

    Fixed tracing;

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,11 @@ import 'antd/dist/antd.min.css';
 import 'react-typist/dist/Typist.css';
 import { wrapper } from '../modules/Store';
 import i18n from '../i18n'
-import { Integrations } from '@sentry/apm';
 import { FC } from 'react';
 
 if (process.env.SENTRY_DSN && process.env.SENTRY_DSN.length > 0) {
     Sentry.init({
         dsn: process.env.SENTRY_DSN,
-        integrations: [
-        	new Integrations.Tracing(),
-        ],
         tracesSampleRate: 0.5,
     });
 }
